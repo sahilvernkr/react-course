43038f27ea299d9f3a1770326bafaff7fa925732
sahil <sahilvernekar96@gmail.com>
2025-02-13 08:45:23 +0530

    optimize customReact.js

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -1,11 +1,13 @@
-function customRender(reactElement, container){
+function customRender(reactElement, container) {
     const domElement = document.createElement(reactElement.type)
     domElement.innerHTML = reactElement.children
-    domElement.setAttribute('href', reactElement.props.href)
-    domElement.setAttribute('target', reactElement.props.target)
+
+    for (const prop in reactElement.props) {
+        if (prop === 'children') continue
+        domElement.setAttribute(prop, reactElement.props[prop])
+    }
     container.appendChild(domElement)
 }
-
 
 const reactElement = {
     type: 'a',
