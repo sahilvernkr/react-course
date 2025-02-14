e785fd05d7c56e855823d67112868698eaa81e88
sahil <sahilvernekar96@gmail.com>
2025-02-14 09:28:16 +0530

    areactELelemnt

--- 01basicvite/src/main.jsx
+++ 01basicvite/src/main.jsx
@@ -1,7 +1,31 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+const reactElement = {
+    type: 'a',
+    props: {
+
+        href: "https://google.com",
+        target: '_blank'
+    },
+    children: 'click me to visit google'
+}
+
+function MyApp(){
+    return (
+        <div>
+            <h1>Custom react App</h1>
+        </div>
+    )
+}
+
+const aReactElement = React.createElement(
+    'a',
+    {href:'https://google.com', target: "_blank"},
+    'click to visit google'
+)
+
 createRoot(document.getElementById('root')).render(
-    <App />
+    aReactElement
 )
