58ff02ce9e9f95d061ed0beee95b9166cdfa0728
sahil <sahilvernekar96@gmail.com>
2025-02-20 08:38:47 +0530

    console counter

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,33 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  let counter = 15
+
+  const addValue = () => {
+    counter +=  1
+    console.log(counter)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>React course with sahil {counter}</h1>
+      <h2>Counter value: {counter}</h2>
+      <button
+      onClick={addValue}
+      >Add Value </button> {" "}
+      <button>Remove Value </button>
+      <p>footer: {counter}</p>
     </>
   )
 }
