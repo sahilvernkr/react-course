9c80caefe1e56740e5c0d539f4b6348d5141416e
sahil <sahilvernekar96@gmail.com>
2025-03-01 19:49:05 +0530

    react functions

--- 03tailwaindprops/src/App.jsx
+++ 03tailwaindprops/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/cards'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <h1 className='text-3xl bg-green-500 p-3 rounded-md'>Vite with tailwaind</h1>
+      <Card />
     </>
   )
 }
---
+++ 03tailwaindprops/src/components/cards.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function cards() {
+  return (
+    <div>
+        <img src="https://i.pinimg.com/originals/9d/f7/12/9df71280ef9bcceb74f507a168ddecee.jpg" alt="" />
+        <h1 className='text-2xl bg-green-500 p-3 rounded'>A card for photos</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas vel illum perspiciatis officia eum iste. Officia, reprehenderit. A accusantium, similique quod tempora fugiat error nostrum, esse quaerat, nam voluptate tempore.</p>
+    </div>
+  )
+}
+
+export default cards
